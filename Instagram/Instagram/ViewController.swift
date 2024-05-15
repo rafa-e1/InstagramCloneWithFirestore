05340beccb284c616365354d17a973aec3daa55c
RAFA <rafa71802937@gmail.com>
2024-05-15 10:59:16 +0900

    init: 프로젝트 생성
    프로젝트 생성 및 초기 설정을 완료했습니다.

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by RAFA on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
