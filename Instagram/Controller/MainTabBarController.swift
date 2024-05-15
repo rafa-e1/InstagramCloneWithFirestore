da323bbbe343e1f9ddc97987644c067070c5e726
RAFA <rafa71802937@gmail.com>
2024-05-15 11:37:24 +0900

    refactor: 중복 코드 제거를 위한 templateNavigationController 함수 생성 및 적용
    - templateNavigationController 함수 생성하여 중복 코드 제거
    - 각 탭의 ViewController 설정 시 반복되는 코드를 템플릿 함수로 대체
    - Feed, Search, ImageSelector, Notification, Profile 컨트롤러를 템플릿 함수 사용하여 연결
    - 코드 가독성 및 유지보수성 향상

--- Instagram/Controller/MainTabBarController.swift
+++ Instagram/Controller/MainTabBarController.swift
@@ -20,17 +20,50 @@ class MainTabBarController: UITabBarController {
     // MARK: - Helpers
     
     func configureViewControllers() {
-        let feed = FeedController()
+        let feed = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "home_unselected"),
+            selectedImage: #imageLiteral(resourceName: "home_selected"),
+            rootViewController: FeedController()
+        )
         
-        let search = SearchController()
+        let search = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "search_unselected"),
+            selectedImage: #imageLiteral(resourceName: "search_selected"),
+            rootViewController: SearchController()
+        )
         
-        let imageSelector = ImageSelectorController()
+        let imageSelector = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "plus_unselected"),
+            selectedImage: #imageLiteral(resourceName: "plus_unselected"),
+            rootViewController: ImageSelectorController()
+        )
         
-        let notification = NotificationController()
+        let notification = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "like_unselected"),
+            selectedImage: #imageLiteral(resourceName: "like_selected"),
+            rootViewController: NotificationController()
+        )
         
-        let profile = ProfileController()
+        let profile = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "profile_unselected"),
+            selectedImage: #imageLiteral(resourceName: "profile_selected"),
+            rootViewController: ProfileController()
+        )
         
         viewControllers = [feed, search, imageSelector, notification, profile]
+    }
+    
+    func templateNavigationController(
+        unselectedImage: UIImage,
+        selectedImage: UIImage,
+        rootViewController: UIViewController
+    ) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        tabBar.tintColor = .black
+        navigationController.tabBarItem.image = unselectedImage
+        navigationController.tabBarItem.selectedImage = selectedImage
+        navigationController.navigationBar.tintColor = .black
+        return navigationController
     }
     
 }
