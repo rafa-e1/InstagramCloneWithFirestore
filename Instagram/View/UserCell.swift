7fbd1d4150b25e94b70343dc0defed7cec6bcde5
RAFA <rafa71802937@gmail.com>
2024-05-25 16:16:27 +0900

    feat: 검색 뷰 UI 구현
    - UITableViewController와 UITableViewCell 사용

---
+++ Instagram/View/UserCell.swift
@@ -0,0 +1,65 @@
+//
+//  UserCell.swift
+//  Instagram
+//
+//  Created by RAFA on 5/25/24.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        return imageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.text = "venom"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.text = "Eddie Brock"
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 48, width: 48)
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.alignment = .leading
+        
+        addSubview(stack)
+        stack.centerY(
+            inView: profileImageView,
+            leftAnchor: profileImageView.rightAnchor,
+            paddingLeft: 8
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
