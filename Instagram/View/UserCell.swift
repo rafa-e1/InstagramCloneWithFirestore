fa2479ce20350f4ec876670a788439f3b4c6017e
RAFA <rafa71802937@gmail.com>
2024-05-25 17:51:41 +0900

    feat: SearchController에 테이블 뷰 셀 구성 및 데이터 바인딩 로직 구현
    - UserService에 Firestore에서 사용자 데이터 비동기 로딩 함수 fetchUsers 추가
    - UserCell에 viewModel 바인딩 및 동적 UI 업데이트 로직 추가
    - SearchController에 비동기 데이터 로딩 및 테이블 뷰 reloadData 로직 구현

--- Instagram/View/UserCell.swift
+++ Instagram/View/UserCell.swift
@@ -11,6 +11,12 @@ class UserCell: UITableViewCell {
     
     // MARK: - Properties
     
+    var viewModel: UserCellViewModel? {
+        didSet {
+            configure()
+        }
+    }
+    
     private let profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -60,6 +66,16 @@ class UserCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageURL)
+        usernameLabel.text = viewModel.username
+        fullnameLabel.text = viewModel.fullname
     }
     
 }
---
+++ Instagram/ViewModel/UserCellViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserCellViewModel.swift
+//  Instagram
+//
+//  Created by RAFA on 5/25/24.
+//
+
+import Foundation
+
+struct UserCellViewModel {
+    private let user: User
+    
+    var profileImageURL: URL? {
+        return URL(string: user.profileImageURL)
+    }
+    
+    var username: String {
+        return user.username
+    }
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
