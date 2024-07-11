512d94d64aaef3bc91e08554edb73353373535c4
RAFA <rafa71802937@gmail.com>
2024-07-11 15:11:11 +0900

    refactor: self 키워드 추가

--- Instagram/Model/User.swift
+++ Instagram/Model/User.swift
@@ -22,12 +22,12 @@ struct User {
     var isCurrentUser: Bool { return Auth.auth().currentUser?.uid == uid }
     
     init(dictionary: [String: Any]) {
-        email = dictionary["email"] as? String ?? ""
-        fullname = dictionary["fullname"] as? String ?? ""
-        profileImageURL = dictionary["profileImageURL"] as? String ?? ""
-        username = dictionary["username"] as? String ?? ""
-        uid = dictionary["uid"] as? String ?? ""
-        stats = UserStats(followers: 0, following: 0)
+        self.email = dictionary["email"] as? String ?? ""
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.profileImageURL = dictionary["profileImageURL"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.stats = UserStats(followers: 0, following: 0)
     }
 }
 
