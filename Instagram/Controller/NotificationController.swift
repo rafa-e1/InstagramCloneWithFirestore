75be22653f70b33081c3f9ca63fcedabbd28f488
RAFA <rafa71802937@gmail.com>
2024-10-20 18:22:19 +0900

    feat: Fetching Notifications

--- Instagram/Controller/NotificationController.swift
+++ Instagram/Controller/NotificationController.swift
@@ -13,12 +13,26 @@ class NotificationController: UITableViewController {
 
     // MARK: - Properties
 
+    private var notifications = [Notification]() {
+        didSet { tableView.reloadData() }
+    }
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTableView()
+        fetchNotifications()
+    }
+
+    // MARK: - API
+
+    func fetchNotifications() {
+        NotificationService.fetchNotifications { notifications in
+            self.notifications = notifications
+            print("DEBUG: \(notifications)")
+        }
     }
 
     // MARK: - Helpers
@@ -35,7 +49,7 @@ class NotificationController: UITableViewController {
 
 extension NotificationController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
 
     override func tableView(
