d641798cbdd1c12f232e7821fa50f5c277dee844
RAFA <rafa71802937@gmail.com>
2024-12-09 23:36:36 +0900

    refactor: CustomTextField의 UI 구성 코드 리팩터링

--- Instagram/View/CustomTextField.swift
+++ Instagram/View/CustomTextField.swift
@@ -7,34 +7,49 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class CustomTextField: UITextField {
-    
+
+    // MARK: - Initializer
+
     init(placeholder: String, isPassword: Bool) {
         super.init(frame: .zero)
         
-        let spacer = UIView()
-        spacer.setDimensions(height: 50, width: 12)
-        leftView = spacer
-        leftViewMode = .always
-        
-        borderStyle = .none
-        textColor = .white
-        keyboardAppearance = .dark
-        keyboardType = .emailAddress
-        backgroundColor = .white.withAlphaComponent(0.1)
-        attributedPlaceholder = NSAttributedString(
-            string: placeholder,
-            attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)]
-        )
-        isSecureTextEntry = isPassword
-        autocapitalizationType = .none
-        autocorrectionType = .no
-        spellCheckingType = .no
-        setHeight(50)
+        configureTextField(placeholder: placeholder, isPassword: isPassword)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    // MARK: - UI Configuration
+
+    private func configureTextField(placeholder: String, isPassword: Bool) {
+        let spacer = UIView()
+        spacer.snp.makeConstraints { $0.width.equalTo(12) }
+
+        self.do {
+            $0.leftView = spacer
+            $0.leftViewMode = .always
+            $0.borderStyle = .none
+            $0.textColor = .white
+            $0.keyboardAppearance = .dark
+            $0.keyboardType = .emailAddress
+            $0.backgroundColor = .white.withAlphaComponent(0.1)
+            $0.isSecureTextEntry = isPassword
+            $0.autocapitalizationType = .none
+            $0.autocorrectionType = .no
+            $0.spellCheckingType = .no
+            $0.attributedPlaceholder = NSAttributedString(
+                string: placeholder,
+                attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)]
+            )
+        }
+
+        self.snp.makeConstraints {
+            $0.height.equalTo(50)
+        }
+    }
 }
