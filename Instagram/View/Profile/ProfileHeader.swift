35042c0a17634d4f83a8a8c85570f6ae13786d4c
RAFA <rafa71802937@gmail.com>
2024-12-11 19:38:24 +0900

    refactor: SnapKit, Then 적용 및 함수 분리

--- Instagram/View/Profile/ProfileHeader.swift
+++ Instagram/View/Profile/ProfileHeader.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 import SDWebImage
+import SnapKit
+import Then
 
 protocol ProfileHeaderDelegate: AnyObject {
     func header(_ profileHeader: ProfileHeader, didTapActionButtonFor user: User)
@@ -18,151 +20,34 @@ final class ProfileHeader: UICollectionReusableView {
     // MARK: - Properties
     
     var viewModel: ProfileHeaderViewModel? {
-        didSet {
-            configure()
-        }
+        didSet { configure() }
     }
     
     weak var delegate: ProfileHeaderDelegate?
     
-    private let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.backgroundColor = .lightGray
-        return imageView
-    }()
-    
-    private let nameLabel: UILabel = {
-        let label = UILabel()
-        label.font = .boldSystemFont(ofSize: 14)
-        return label
-    }()
-    
-    private lazy var editProfileFollowButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Loading", for: .normal)
-        button.layer.cornerRadius = 3
-        button.layer.borderWidth = 0.5
-        button.layer.borderColor = UIColor.lightGray.cgColor
-        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(
-            self,
-            action: #selector(handleEditProfileFollowTapped),
-            for: .touchUpInside
-        )
-        return button
-    }()
-    
-    private lazy var postsLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var followersLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private lazy var followingLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let gridButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "grid"), for: .normal)
-        return button
-    }()
-    
-    let listButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "list"), for: .normal)
-        button.tintColor = UIColor(white: 0, alpha: 0.2)
-        return button
-    }()
-    
-    let bookmarkButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "ribbon"), for: .normal)
-        button.tintColor = UIColor(white: 0, alpha: 0.2)
-        return button
-    }()
-    
+    private let profileImageView = UIImageView()
+    private let nameLabel = UILabel()
+    private let editProfileFollowButton = UIButton(type: .system)
+    private let postsLabel = UILabel()
+    private let followersLabel = UILabel()
+    private let followingLabel = UILabel()
+    private let profileStatsStackView = UIStackView()
+    private let topDividerView = UIView()
+    private let gridButton = UIButton(type: .system)
+    private let listButton = UIButton(type: .system)
+    private let bookmarkButton = UIButton(type: .system)
+    private let contentSwitchStackView = UIStackView()
+    private let bottomDividerView = UIView()
+
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        addSubview(profileImageView)
-        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
-        profileImageView.setDimensions(height: 80, width: 80)
-        profileImageView.layer.cornerRadius = 80 / 2
-        
-        addSubview(nameLabel)
-        nameLabel.anchor(
-            top: profileImageView.bottomAnchor,
-            left: leftAnchor,
-            paddingTop: 12,
-            paddingLeft: 12
-        )
-        
-        addSubview(editProfileFollowButton)
-        editProfileFollowButton.anchor(
-            top: nameLabel.bottomAnchor,
-            left: leftAnchor,
-            right: rightAnchor,
-            paddingTop: 16,
-            paddingLeft: 24,
-            paddingRight: 24
-        )
-        
-        let stack = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingLabel])
-        stack.distribution = .fillEqually
-        
-        addSubview(stack)
-        stack.centerY(inView: profileImageView)
-        stack.anchor(
-            left: profileImageView.rightAnchor,
-            right: rightAnchor,
-            paddingLeft: 12,
-            paddingRight: 12,
-            height: 50
-        )
-        
-        let topDivider = UIView()
-        topDivider.backgroundColor = .lightGray
-        
-        let bottomDivider = UIView()
-        bottomDivider.backgroundColor = .lightGray
-        
-        let buttonStack = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
-        buttonStack.distribution = .fillEqually
-        
-        addSubview(buttonStack)
-        addSubview(topDivider)
-        addSubview(bottomDivider)
-        
-        buttonStack.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
-        topDivider.anchor(
-            top: buttonStack.topAnchor,
-            left: leftAnchor,
-            right: rightAnchor,
-            height: 0.5
-        )
-        bottomDivider.anchor(
-            top: buttonStack.bottomAnchor,
-            left: leftAnchor,
-            right: rightAnchor,
-            height: 0.5
-        )
+
+        setAddTargets()
+        setStyle()
+        setHierarchy()
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -191,5 +76,120 @@ final class ProfileHeader: UICollectionReusableView {
         postsLabel.attributedText = viewModel.numberOfPosts
         followersLabel.attributedText = viewModel.numberOfFollowers
         followingLabel.attributedText = viewModel.numberOfFollowing
+    }
+
+    private func setAddTargets() {
+        editProfileFollowButton.addTarget(
+            self,
+            action: #selector(handleEditProfileFollowTapped),
+            for: .touchUpInside
+        )
+    }
+
+    // MARK: - UI
+
+    private func setStyle() {
+        profileImageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.backgroundColor = .lightGray
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 80 / 2
+        }
+
+        nameLabel.font = .boldSystemFont(ofSize: 14)
+
+        editProfileFollowButton.do {
+            $0.setTitle("Loading", for: .normal)
+            $0.layer.cornerRadius = 3
+            $0.layer.borderWidth = 0.5
+            $0.layer.borderColor = UIColor.lightGray.cgColor
+            $0.titleLabel?.font = .boldSystemFont(ofSize: 14)
+            $0.setTitleColor(.black, for: .normal)
+        }
+
+        [postsLabel, followersLabel, followingLabel].forEach {
+            $0.numberOfLines = 0
+            $0.textAlignment = .center
+        }
+
+        profileStatsStackView.configureStackView(
+            addArrangedSubviews: postsLabel, followersLabel, followingLabel,
+            axis: .horizontal
+        )
+
+        gridButton.setImage(UIImage(named: "grid"), for: .normal)
+
+        listButton.do {
+            $0.setImage(UIImage(named: "list"), for: .normal)
+            $0.tintColor = UIColor(white: 0, alpha: 0.2)
+        }
+
+        bookmarkButton.do {
+            $0.setImage(UIImage(named: "ribbon"), for: .normal)
+            $0.tintColor = UIColor(white: 0, alpha: 0.2)
+        }
+
+        contentSwitchStackView.configureStackView(
+            addArrangedSubviews: gridButton, listButton, bookmarkButton,
+            axis: .horizontal
+        )
+
+        [topDividerView, bottomDividerView].forEach {
+            $0.backgroundColor = .lightGray
+        }
+    }
+
+    private func setHierarchy() {
+        addSubviews(
+            profileImageView,
+            nameLabel,
+            profileStatsStackView,
+            editProfileFollowButton,
+            topDividerView,
+            contentSwitchStackView,
+            bottomDividerView
+        )
+    }
+
+    private func setLayout() {
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(16)
+            $0.leading.equalTo(12)
+            $0.size.equalTo(80)
+        }
+
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(12)
+            $0.leading.equalTo(profileImageView)
+        }
+
+        editProfileFollowButton.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+        }
+
+        profileStatsStackView.snp.makeConstraints {
+            $0.centerY.equalTo(profileImageView)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(12)
+            $0.trailing.equalTo(-12)
+            $0.height.equalTo(50)
+        }
+
+        topDividerView.snp.makeConstraints {
+            $0.top.equalTo(contentSwitchStackView.snp.top)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(0.5)
+        }
+
+        contentSwitchStackView.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+
+        bottomDividerView.snp.makeConstraints {
+            $0.top.equalTo(contentSwitchStackView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(0.5)
+        }
     }
 }
