ba5e7c8b042667eacfa29d9639f6ae1f08dbbb28
RAFA <rafa71802937@gmail.com>
2024-12-18 13:34:37 +0900

    refactor: 코드 컨벤션 반영 및 함수 분리

--- Instagram/View/CommentCell.swift
+++ Instagram/View/CommentCell.swift
@@ -7,54 +7,54 @@
 
 import UIKit
 
-final class CommentCell: UICollectionViewCell {
+final class CommentCell: BaseCollectionViewCell {
 
     // MARK: - Properties
-    
+
     var viewModel: CommentViewModel? {
         didSet { configure() }
     }
-    
-    private let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.backgroundColor = .lightGray
-        return imageView
-    }()
-    
+
+    private let profileImageView = UIImageView()
     private let commentLabel = UILabel()
-    
-    // MARK: - Lifecycle
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        addSubview(profileImageView)
-        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 8)
-        profileImageView.setDimensions(height: 40, width: 40)
-        profileImageView.layer.cornerRadius = 40 / 2
-        
-        commentLabel.numberOfLines = 0
-        addSubview(commentLabel)
-        commentLabel.centerY(
-            inView: profileImageView,
-            leftAnchor: profileImageView.rightAnchor,
-            paddingLeft: 8
-        )
-        commentLabel.anchor(right: rightAnchor, paddingRight: 8)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     // MARK: - Helpers
-    
+
     func configure() {
         guard let viewModel = viewModel else { return }
-        
+
         profileImageView.sd_setImage(with: viewModel.profileImageURL)
         commentLabel.attributedText = viewModel.commentLabelText()
+    }
+
+    // MARK: - UI
+
+    override func setStyle() {
+        profileImageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.backgroundColor = .lightGray
+            $0.layer.cornerRadius = 40 / 2
+            $0.clipsToBounds = true
+        }
+
+        commentLabel.numberOfLines = 0
+    }
+
+    override func setHierarchy() {
+        contentView.addSubviews(profileImageView, commentLabel)
+    }
+
+    override func setLayout() {
+        profileImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(8)
+            $0.size.equalTo(40)
+        }
+
+        commentLabel.snp.makeConstraints {
+            $0.centerY.equalTo(profileImageView)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(8)
+            $0.trailing.equalTo(-8)
+        }
     }
 }
