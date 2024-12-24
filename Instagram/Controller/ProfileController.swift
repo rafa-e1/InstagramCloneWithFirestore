92a822d9e743f25d3d0e2f88a2cda9ce1b5d20d0
RAFA <rafa71802937@gmail.com>
2024-12-25 00:16:04 +0900

    refactor: BaseViewController 적용
    - Merry Christmas

--- Instagram/Controller/ProfileController.swift
+++ Instagram/Controller/ProfileController.swift
@@ -7,68 +7,67 @@
 
 import UIKit
 
-private let reuseIdentifier = "ProfileCell"
+final class ProfileController: BaseViewController {
 
-
-final class ProfileController: UICollectionViewController {
-    
     // MARK: - Properties
-    
+
     private var user: User
     private var posts = [Post]()
-    
-    // MARK: - Lifecycle
-    
+
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+
+    // MARK: - Initializer
+
     init(user: User) {
         self.user = user
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configureCollectionView()
+
+        registerCells()
+        setDelegates()
         checkIfUserIsFollowed()
         fetchUserStats()
         fetchPosts()
     }
-    
+
     // MARK: - API
-    
+
     func checkIfUserIsFollowed() {
         UserService.checkIfUserIsFollowed(uid: user.uid) { isFollowed in
             self.user.isFollowed = isFollowed
             self.collectionView.reloadData()
         }
     }
-    
+
     func fetchUserStats() {
         UserService.fetchUserStats(uid: user.uid) { stats in
             self.user.stats = stats
             self.collectionView.reloadData()
-            
+
             print("DEBUG: \(stats)")
         }
     }
-    
+
     func fetchPosts() {
         PostService.fetchPosts(forUser: user.uid) { posts in
             self.posts = posts
             self.collectionView.reloadData()
         }
     }
-    
-    // MARK: - Helpers
-    
-    func configureCollectionView() {
-        navigationItem.title = user.username
-        collectionView.backgroundColor = .white
-    }
 
+    // MARK: - Helpers
 
     private func registerCells() {
         collectionView.register(
@@ -82,54 +81,80 @@ final class ProfileController: UICollectionViewController {
             withReuseIdentifier: ProfileHeader.identifier
         )
     }
+
+    private func setDelegates() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+    // MARK: - UI
+
+    override func setStyle() {
+        navigationItem.title = user.username
+        collectionView.backgroundColor = .white
+    }
+
+    override func setHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    override func setLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
 
 // MARK: - UICollectionViewDataSource
 
-extension ProfileController {
+extension ProfileController: UICollectionViewDataSource {
 
-    override func collectionView(
+    func collectionView(
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
     ) -> Int {
         return posts.count
     }
-    
-    override func collectionView(
+
+    func collectionView(
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: reuseIdentifier,
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ProfileCell.identifier,
             for: indexPath
-        ) as! ProfileCell
+        ) as? ProfileCell else {
+            return UICollectionViewCell()
+        }
+
         cell.viewModel = PostViewModel(post: posts[indexPath.row])
+
         return cell
     }
-    
-    override func collectionView(
+
+    func collectionView(
         _ collectionView: UICollectionView,
         viewForSupplementaryElementOfKind kind: String,
         at indexPath: IndexPath
     ) -> UICollectionReusableView {
         guard let header = collectionView.dequeueReusableSupplementaryView(
             ofKind: kind,
-            withReuseIdentifier: headerIdentifier,
+            withReuseIdentifier: ProfileHeader.identifier,
             for: indexPath
         ) as? ProfileHeader else { return UICollectionReusableView() }
-        
+
         header.delegate = self
         header.viewModel = ProfileHeaderViewModel(user: user)
-        
+
         return header
     }
 }
 
 // MARK: - UICollectionViewDelegate
 
-extension ProfileController {
+extension ProfileController: UICollectionViewDelegate {
 
-    override func collectionView(
+    func collectionView(
         _ collectionView: UICollectionView,
         didSelectItemAt indexPath: IndexPath
     ) {
@@ -150,7 +175,7 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     ) -> CGFloat {
         return 1
     }
-    
+
     func collectionView(
         _ collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
@@ -158,7 +183,7 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
     ) -> CGFloat {
         return 1
     }
-    
+
     func collectionView(
         _ collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
@@ -167,7 +192,7 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
         let width = (view.frame.width - 2) / 3
         return CGSize(width: width, height: width)
     }
-    
+
     func collectionView(
         _ collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
@@ -180,7 +205,7 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
 // MARK: - ProfileHeaderDelegate
 
 extension ProfileController: ProfileHeaderDelegate {
-    
+
     func header(_ profileHeader: ProfileHeader, didTapActionButtonFor user: User) {
         guard let tab = tabBarController as? MainTabBarController else { return }
         guard let currentUser = tab.user else { return }
@@ -191,7 +216,7 @@ extension ProfileController: ProfileHeaderDelegate {
             UserService.unfollow(uid: user.uid) { error in
                 self.user.isFollowed = false
                 self.collectionView.reloadData()
-                
+
                 PostService.updateUserFeedAfterFollowing(user: user, didFollow: false)
             }
         } else {
